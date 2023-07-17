2c8d8319e6c4a6ee8d651a568912bd1f2f340082
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-17 14:01:25 +0300

    Update videolimit.py

--- AarohiX/plugins/videolimit.py
+++ AarohiX/plugins/videolimit.py
@@ -2,10 +2,10 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from strings import get_command
-from AarohiX import app
-from AarohiX.misc import SUDOERS
-from AarohiX.utils.database import set_video_limit
-from AarohiX.utils.decorators.language import language
+from AnonX import app
+from AnonX.misc import SUDOERS
+from AnonX.utils.database import set_video_limit
+from AnonX.utils.decorators.language import language
 
 VIDEOLIMIT_COMMAND = get_command("VIDEOLIMIT_COMMAND")
 
