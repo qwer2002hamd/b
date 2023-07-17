f5d913ab33efde0cde06e800007f8483bc3cfa74
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-17 14:29:12 +0300

    Update play.py

--- AarohiX/utils/decorators/play.py
+++ AarohiX/utils/decorators/play.py
@@ -2,15 +2,15 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from config import PLAYLIST_IMG_URL, PRIVATE_BOT_MODE, adminlist
 from strings import get_string
-from AarohiX import YouTube, app
-from AarohiX.misc import SUDOERS
-from AarohiX.utils.database import (get_cmode, get_lang,
+from AnonX import YouTube, app
+from AnonX.misc import SUDOERS
+from AnonX.utils.database import (get_cmode, get_lang,
                                        get_playmode, get_playtype,
                                        is_active_chat,
                                        is_commanddelete_on,
                                        is_served_private_chat)
-from AarohiX.utils.database.memorydatabase import is_maintenance
-from AarohiX.utils.inline.playlist import botplaylist_markup
+from AnonX.utils.database.memorydatabase import is_maintenance
+from AnonX.utils.inline.playlist import botplaylist_markup
 
 
 def PlayWrapper(command):
