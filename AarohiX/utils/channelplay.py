0952590ac395f91a1c299cc146d4ec4953ee13a9
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-17 14:37:12 +0300

    Update channelplay.py

--- AarohiX/utils/channelplay.py
+++ AarohiX/utils/channelplay.py
@@ -1,5 +1,5 @@
-from AarohiX import app
-from AarohiX.utils.database import get_cmode
+from AnonX import app
+from AnonX.utils.database import get_cmode
 
 
 async def get_channeplayCB(_, command, CallbackQuery):
