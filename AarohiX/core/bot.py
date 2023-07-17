4c66f0a938dd219741169c66707f318326722521
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-17 12:10:21 +0300

    Update bot.py

--- AarohiX/core/bot.py
+++ AarohiX/core/bot.py
@@ -8,11 +8,11 @@ from ..logging import LOGGER
 
 
 
-class AarohiXBot(Client):
+class AnonXBot(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
-            "AarohiXMusic",
+            "AnonXMusic",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
