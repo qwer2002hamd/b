f834904a84b971158f28053f9ddc7c0f53e4b1d0
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-17 13:11:44 +0300

    Update help.py

--- AarohiX/plugins/help.py
+++ AarohiX/plugins/help.py
@@ -6,13 +6,13 @@ from pyrogram.types import InlineKeyboardMarkup, Message
 import config
 from config import BANNED_USERS
 from strings import get_command, get_string, helpers
-from AarohiX import app
-from AarohiX.misc import SUDOERS
-from AarohiX.utils import help_pannel
-from AarohiX.utils.database import get_lang, is_commanddelete_on
-from AarohiX.utils.decorators.language import (LanguageStart,
+from AnonX import app
+from AnonX.misc import SUDOERS
+from AnonX.utils import help_pannel
+from AnonX.utils.database import get_lang, is_commanddelete_on
+from AnonX.utils.decorators.language import (LanguageStart,
                                                   languageCB)
-from AarohiX.utils.inline.help import (help_back_markup,
+from AnonX.utils.inline.help import (help_back_markup,
                                           private_help_panel)
 
 ### Command
