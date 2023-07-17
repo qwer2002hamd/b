9b545bc8e807c027c4583b30db79427deb8155d0
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-17 13:44:32 +0300

    Update shuffle.py

--- AarohiX/plugins/shuffle.py
+++ AarohiX/plugins/shuffle.py
@@ -5,9 +5,9 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from AarohiX import app
-from AarohiX.misc import db
-from AarohiX.utils.decorators import AdminRightsCheck
+from AnonX import app
+from AnonX.misc import db
+from AnonX.utils.decorators import AdminRightsCheck
 
 # Commands
 SHUFFLE_COMMAND = get_command("SHUFFLE_COMMAND")
