5eed05c0c2316ea215d780f11717c9dcc51f05bd
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-17 13:51:47 +0300

    Update stop.py

--- AarohiX/plugins/stop.py
+++ AarohiX/plugins/stop.py
@@ -3,10 +3,10 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from AarohiX import app
-from AarohiX.core.call import Aarohi
-from AarohiX.utils.database import set_loop
-from AarohiX.utils.decorators import AdminRightsCheck
+from AnonX import app
+from AnonX.core.call import Anon
+from AnonX.utils.database import set_loop
+from AnonX.utils.decorators import AdminRightsCheck
 
 # Commands
 STOP_COMMAND = get_command("STOP_COMMAND")
@@ -22,7 +22,7 @@ STOP_COMMAND = get_command("STOP_COMMAND")
 async def stop_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return await message.reply_text(_["general_2"])
-    await Aarohi.stop_stream(chat_id)
+    await Anon.stop_stream(chat_id)
     await set_loop(chat_id, 0)
     await message.reply_text(
         _["admin_9"].format(message.from_user.mention)
