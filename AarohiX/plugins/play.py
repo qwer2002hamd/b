53b6433221672cffbe80a6e3b6813956dde45da1
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-17 13:26:18 +0300

    Update play.py

--- AarohiX/plugins/play.py
+++ AarohiX/plugins/play.py
@@ -10,21 +10,21 @@ from pytgcalls.exceptions import NoActiveGroupCall
 import config
 from config import BANNED_USERS, lyrical
 from strings import get_command
-from AarohiX import (Apple, Resso, SoundCloud, Spotify, Telegram,
+from AnonX import (Apple, Resso, SoundCloud, Spotify, Telegram,
                         YouTube, app)
-from AarohiX.core.call import Aarohi
-from AarohiX.utils import seconds_to_min, time_to_seconds
-from AarohiX.utils.channelplay import get_channeplayCB
-from AarohiX.utils.database import is_video_allowed
-from AarohiX.utils.decorators.language import languageCB
-from AarohiX.utils.decorators.play import PlayWrapper
-from AarohiX.utils.formatters import formats
-from AarohiX.utils.inline.play import (livestream_markup,
+from AnonX.core.call import Anon
+from AnonX.utils import seconds_to_min, time_to_seconds
+from AnonX.utils.channelplay import get_channeplayCB
+from AnonX.utils.database import is_video_allowed
+from AnonX.utils.decorators.language import languageCB
+from AnonX.utils.decorators.play import PlayWrapper
+from AnonX.utils.formatters import formats
+from AnonX.utils.inline.play import (livestream_markup,
                                           playlist_markup,
                                           slider_markup, track_markup)
-from AarohiX.utils.inline.playlist import botplaylist_markup
-from AarohiX.utils.logger import play_logs
-from AarohiX.utils.stream.stream import stream
+from AnonX.utils.inline.playlist import botplaylist_markup
+from AnonX.utils.logger import play_logs
+from AnonX.utils.stream.stream import stream
 
 # Command
 PLAY_COMMAND = get_command("PLAY_COMMAND")
