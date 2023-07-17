e7ea2ef56705cb93db25aa1e40cc685708645555
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-17 13:39:53 +0300

    Update resume.py

--- AarohiX/plugins/resume.py
+++ AarohiX/plugins/resume.py
@@ -3,10 +3,10 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from AarohiX import app
-from AarohiX.core.call import Aarohi
-from AarohiX.utils.database import is_music_playing, music_on
-from AarohiX.utils.decorators import AdminRightsCheck
+from AnonX import app
+from AnonX.core.call import Anon
+from AnonX.utils.database import is_music_playing, music_on
+from AnonX.utils.decorators import AdminRightsCheck
 
 # Commands
 RESUME_COMMAND = get_command("RESUME_COMMAND")
@@ -25,7 +25,7 @@ async def resume_com(cli, message: Message, _, chat_id):
     if await is_music_playing(chat_id):
         return await message.reply_text(_["admin_3"])
     await music_on(chat_id)
-    await Aarohi.resume_stream(chat_id)
+    await Anon.resume_stream(chat_id)
     await message.reply_text(
         _["admin_4"].format(message.from_user.mention)
     )
