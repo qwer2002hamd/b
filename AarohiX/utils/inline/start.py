c44bb35fa8c629846fadc9c712eed9c7707239cb
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-17 14:31:18 +0300

    Update start.py

--- AarohiX/utils/inline/start.py
+++ AarohiX/utils/inline/start.py
@@ -3,7 +3,7 @@ from typing import Union
 from pyrogram.types import InlineKeyboardButton
 
 import config
-from AarohiX import app
+from AnonX import app
 
 
 def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
