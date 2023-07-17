e79377ff41882f5ad4fecdd907064b8a4dc4a890
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-17 12:55:14 +0300

    Update autoend.py

--- AarohiX/plugins/autoend.py
+++ AarohiX/plugins/autoend.py
@@ -2,10 +2,10 @@ from pyrogram import filters
 
 import config
 from strings import get_command
-from AarohiX import app
-from AarohiX.misc import SUDOERS
-from AarohiX.utils.database import autoend_off, autoend_on
-from AarohiX.utils.decorators.language import language
+from AnonX import app
+from AnonX.misc import SUDOERS
+from AnonX.utils.database import autoend_off, autoend_on
+from AnonX.utils.decorators.language import language
 
 # Commands
 AUTOEND_COMMAND = get_command("AUTOEND_COMMAND")
