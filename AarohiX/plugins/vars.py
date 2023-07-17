9b3272b8ddad946328f68d53ef10f843585de51c
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-17 14:00:57 +0300

    Update vars.py

--- AarohiX/plugins/vars.py
+++ AarohiX/plugins/vars.py
@@ -4,10 +4,10 @@ from pyrogram import filters
 
 import config
 from strings import get_command
-from AarohiX import app
-from AarohiX.misc import SUDOERS
-from AarohiX.utils.database.memorydatabase import get_video_limit
-from AarohiX.utils.formatters import convert_bytes
+from AnonX import app
+from AnonX.misc import SUDOERS
+from AnonX.utils.database.memorydatabase import get_video_limit
+from AnonX.utils.formatters import convert_bytes
 
 VARS_COMMAND = get_command("VARS_COMMAND")
 
