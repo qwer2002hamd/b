fe065a71140df674a9b1f1791844d0d4e3432b73
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-17 13:17:53 +0300

    Update inline.py

--- AarohiX/plugins/inline.py
+++ AarohiX/plugins/inline.py
@@ -4,8 +4,8 @@ from pyrogram.types import (InlineKeyboardButton,
 from youtubesearchpython.__future__ import VideosSearch
 
 from config import BANNED_USERS, MUSIC_BOT_NAME
-from AarohiX import app
-from AarohiX.utils.inlinequery import answer
+from AnonX import app
+from AnonX.utils.inlinequery import answer
 
 
 @app.on_inline_query(~BANNED_USERS)
