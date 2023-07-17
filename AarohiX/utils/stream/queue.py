3ca30b1c56aa22e1ea72b870b84aa7f41978174b
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-17 14:32:30 +0300

    Update queue.py

--- AarohiX/utils/stream/queue.py
+++ AarohiX/utils/stream/queue.py
@@ -2,7 +2,7 @@ from typing import Union
 
 from config import autoclean, chatstats, userstats
 from config.config import time_to_seconds
-from AarohiX.misc import db
+from AnonX.misc import db
 
 
 async def put_queue(
